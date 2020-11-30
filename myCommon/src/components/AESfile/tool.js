89f2c8b6e36426780d4a16ca87aafebf47d2026e
18271411378@163.com <18271411378@163.com>
2020-11-30 19:50:04 +0800

    1

--- myCommon/src/components/AESfile/tool.js
+++ myCommon/src/components/AESfile/tool.js
@@ -1,6 +1,9 @@
 // base64转downloadfile
+// 使用
+// 下载文件
+//   dataURLtoDownload('base64数据','文件名.txt')
 export function dataURLtoDownload(dataurl, name) {
-  let blob = dataURLtoBlob(dataurl)
+  let blob = base64toBlob(dataurl)
   let url = URL.createObjectURL(blob)
   const a = document.createElement('a'); // 创建a标签
   a.setAttribute('download', name); // download属性
@@ -9,7 +12,7 @@ export function dataURLtoDownload(dataurl, name) {
 }
 // base64转Blob
 
-function dataURLtoBlob(dataurl) {
+function base64toBlob(dataurl) {
   var arr = dataurl.split(','),
     mime = arr[0].match(/:(.*?);/)[1],
     bstr = atob(arr[1]),
@@ -22,6 +25,18 @@ function dataURLtoBlob(dataurl) {
     type: mime
   });
 }
-// 使用
-// 下载文件
-//   dataURLtoDownload('base64数据','文件名.txt')
+
+//url转blob
+export function urlToBase64(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.responseType = "";
+    xhr.onload = function () {
+      if (this.status == 200) {
+        resolve(this.response)
+      }
+    };
+    xhr.send();
+  })
+}
--- myCommon/src/views/test.vue
+++ myCommon/src/views/test.vue
@@ -1,12 +1,43 @@
 <template>
-  <MyUpload />
+  <div></div>
 </template>
 
 <script>
   import MyUpload from '@/components/AESfile/myUpload'
+  import {
+    urlToBase64
+  } from '@/components/AESfile/tool'
+  import {
+    Decrypt,
+    Encrypt
+  } from '@/components/AESfile/AES.js'
   export default {
     components: {
       MyUpload
+    },
+    data() {
+      return {
+        keyFrom: {},
+        afterSrc: 'api/ycuploads/v1/images/202011/20201130/20201130104009_XB1TJZcx.jpg',
+        src: ''
+      }
+    },
+    created() {
+      this.keyFrom = JSON.parse(localStorage.getItem('myKey'))
+    },
+    mounted() {
+      urlToBase64(this.afterSrc).then(res => {
+        this.src = this.jiemi(res)
+        console.log(this.src)
+      })
+    },
+    methods: {
+      // -------解密消息--------
+      jiemi(val) {
+        let arr = val.split('&gzqx')
+        arr[1] = Decrypt(arr[1], this.keyFrom.publicKey, this.keyFrom.privateKey)
+        return arr[0] + arr[1] + arr[2]
+      },
     }
 
   }
